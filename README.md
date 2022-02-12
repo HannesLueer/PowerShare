a0c87c6a2e650e41c6c2340de9ec0585f633662b
Hannes Lüer <hannes-lueer@outlook.de>
2022-02-12 21:16:11 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PowerShare
+
+Neben kommerziellen Ladestationen finden private Lademöglichkeiten (Wallboxen) zunehmend Verbreitung. Kommerzielle Ladestationen können über diverse Apps gefunden werden; dies gilt jedoch nicht für private Wallboxen. Zur Erhöhung der Ladeinfrastrukturdichte sollen auch private Wallboxen für E-Mobilisten zugänglich gemacht werden. Daher soll ein Programm entwickelt werden, welches über mobile Endgeräte ermöglicht, private Ladestationen zu finden und ein Nutzungsentgelt für die Energieentnahme zwischen den Nutzern auszutauschen.
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/img/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+    </div>
+  </header>
+      <router-view></router-view>
+</template>
+
+<style>
+@import "@/assets/css/base.css";
+
+</style>
